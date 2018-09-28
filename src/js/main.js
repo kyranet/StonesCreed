4814c6942b79ea36fa36ff7e355aba1efc8be17c
kyranet <kyradiscord@gmail.com>
2018-09-28 18:42:59 +0200

    lint: Change jshint to eslint

--- src/js/main.js
+++ src/js/main.js
@@ -4,39 +4,39 @@ var PlayScene = require('./play_scene.js');
 
 
 var BootScene = {
-  preload: function () {
-    // load here assets required for the loading screen
-    this.game.load.image('preloader_bar', 'images/preloader_bar.png');
-  },
+	preload() {
+		// load here assets required for the loading screen
+		this.game.load.image('preloader_bar', 'images/preloader_bar.png');
+	},
 
-  create: function () {
-    this.game.state.start('preloader');
-  }
+	create() {
+		this.game.state.start('preloader');
+	}
 };
 
 
 var PreloaderScene = {
-  preload: function () {
-    this.loadingBar = this.game.add.sprite(0, 240, 'preloader_bar');
-    this.loadingBar.anchor.setTo(0, 0.5);
-    this.load.setPreloadSprite(this.loadingBar);
+	preload() {
+		this.loadingBar = this.game.add.sprite(0, 240, 'preloader_bar');
+		this.loadingBar.anchor.setTo(0, 0.5);
+		this.load.setPreloadSprite(this.loadingBar);
 
-    // TODO: load here the assets for the game
-    this.game.load.image('logo', 'images/phaser.png');
-  },
+		// TODO: load here the assets for the game
+		this.game.load.image('logo', 'images/phaser.png');
+	},
 
-  create: function () {
-    this.game.state.start('play');
-  }
+	create() {
+		this.game.state.start('play');
+	}
 };
 
 
-window.onload = function () {
-  var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game');
+window.onload = function onload() {
+	var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game');
 
-  game.state.add('boot', BootScene);
-  game.state.add('preloader', PreloaderScene);
-  game.state.add('play', PlayScene);
+	game.state.add('boot', BootScene);
+	game.state.add('preloader', PreloaderScene);
+	game.state.add('play', PlayScene);
 
-  game.state.start('boot');
+	game.state.start('boot');
 };
