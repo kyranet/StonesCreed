d2a72527909bb7f53043561091595e8db0b53f31
kyranet <kyradiscord@gmail.com>
2018-12-17 00:22:18 +0100

    ci: Added deploy

---
+++ .travis-deploy.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Based on https://github.com/discordjs/discord.js-site/blob/master/deploy/deploy.sh
+
+set -e
+
+if [ "$TRAVIS_BRANCH" != "master" -o -n "$TRAVIS_TAG" -o "$TRAVIS_PULL_REQUEST" != "false" ]; then
+  echo -e "Not building for a non branch push - building without deploying."
+  yarn run bundle
+  exit 0
+fi
+
+echo -e "Building for a branch push - building and deploying."
+
+REPO=$(git config remote.origin.url)
+SHA=$(git rev-parse --verify HEAD)
+
+TARGET_BRANCH="gh-pages"
+git clone $REPO dist -b $TARGET_BRANCH
+
+yarn run bundle
+
+cd dist
+git add --all .
+git config user.name "Travis CI"
+git config user.email "${COMMIT_EMAIL}"
+git commit -m "Game Build: ${SHA}" || true
+git push "https://${GH_TOKEN}@${GH_REF}" $TARGET_BRANCH
