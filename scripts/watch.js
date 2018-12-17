df4446b35d2701656040fad4abaf304e2208184c
kyranet <kyradiscord@gmail.com>
2018-12-17 12:17:35 +0100

    build: Fix watch script

--- scripts/watch.js
+++ scripts/watch.js
@@ -9,7 +9,11 @@ const ansiCursorUp = '\u001b[1A\u001b[K'
 watch(libPath, { recursive: true }, (event, filename) => {
 	console.log('Watch:', event, filename);
 	if (statSync(join(libPath, filename)).isFile) {
-		execSync('yarn bundle', (error) => error && console.error(error));
-		console.log(`${ansiCursorUp}Watch: Successfully compiled.`);
+		try {
+			execSync('yarn bundle');
+			console.log(`${ansiCursorUp}Watch: Successfully compiled.`);
+		} catch (error) {
+			console.error(`${ansiCursorUp}Watch: ERROR! ${error}`);
+		}
 	}
 });
