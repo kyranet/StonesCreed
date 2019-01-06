73f3848502acc910366f4f8a9b4b601f95017b15
kyraNET <kyradiscord@gmail.com>
2019-01-06 12:06:02 +0100

    src: Turn the loading bar into Phaser.Graphics, joined Boot & Preloader

--- src/lib/main.ts
+++ src/lib/main.ts
@@ -2,17 +2,16 @@ import { BootState } from './states/BootState';
 import { MenuState } from './states/MenuState';
 import { NewGameState } from './states/NewGameState';
 import { PlayState } from './states/PlayState';
-import { PreloaderState } from './states/PreloaderState';
 import { WIN_HEIGHT, WIN_WIDTH } from './util/constants';
 
 window.onload = function onload() {
 	const game = new Phaser.Game(WIN_WIDTH, WIN_HEIGHT, Phaser.AUTO, 'game');
 
 	game.state.add('boot', BootState);
-	game.state.add('preloader', PreloaderState);
 	game.state.add('play', PlayState);
 	game.state.add('menu', MenuState);
 	game.state.add('newGame', NewGameState);
 
+	// Start boot state
 	game.state.start('boot');
 };
--- src/lib/states/BootState.ts
+++ src/lib/states/BootState.ts
@@ -1,14 +1,43 @@
 import { PathFinderPlugin } from '../plugins/PathFinderPlugin';
+import { LOADING_POSITION, WIN_WIDTH } from '../util/constants';
 import { GameState } from './GameState';
 
 export class BootState extends GameState {
+	private loadingBar: Phaser.Graphics = null;
+
 	public preload() {
-		// load here assets required for the loading screen
-		this.game.load.image('preloader_bar', 'images/preloader_bar.png');
+		// this.loadingBar = this.game.add.sprite(0, 240, 'preloader_bar');
+		this.loadingBar = this.game.add.graphics(LOADING_POSITION.x, LOADING_POSITION.y);
+		this.loadingBar.beginFill(0xFFFFFF);
+
+		this.game.load.onFileComplete.add((progress: number) => {
+			this.loadingBar.drawRect(0, 0, (progress / 100) * WIN_WIDTH, 5);
+		});
+
+		this.game.load.onLoadComplete.add(() => {
+			this.loadingBar.endFill();
+		});
+
+		this.game.load.spritesheet('player', 'images/player.png', 48, 96);
+		this.game.load.spritesheet('enemy', 'images/enemy.png', 48, 96);
+		this.game.load.spritesheet('objects', 'images/objects.png', 48, 48);
+		this.game.load.tilemap('Level-0', 'json/Level-0.json', null, Phaser.Tilemap.TILED_JSON);
+		this.game.load.image('overworld', 'images/overworld.png');
+		this.game.load.image('cave', 'images/cave.png');
+		this.game.load.image('logo', 'images/StonesCreed.png');
 	}
 
 	public create() {
 		this.game.pathFinder = this.game.plugins.add(PathFinderPlugin);
-		this.game.state.start('preloader');
+		this.game.state.start('menu');
 	}
+
+	public shutdown(game: Phaser.Game) {
+		if (this.loadingBar) {
+			this.loadingBar.destroy(true);
+			this.loadingBar = null;
+		}
+		return super.shutdown(game);
+	}
+
 }
